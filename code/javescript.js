7274aaf284c68481c7ec653c64928046000b91dc
EmiliePoulsen <eaa24eamp@students.eaaa.dk>
2025-03-09 22:08:51 +0100

    for loop

--- code/javescript.js
+++ code/javescript.js
@@ -10,13 +10,15 @@ function Album (albumName, artistName, rating, productionYear) {
 }
 
 //når man bruger async function skal man være sikker på
-//data'en kommer ind først - man bruger metoden .then
+//data'en kommer ind først - der bruges metoden .then
 fetchContent("albums.json").then((albums) => {
 
   //bruger loop til at sætte ind i array
 let albumObjects = []
 
 //addere først med +1 efter hele loopet er kørt igennem
+//for at undgå hardcoding bruges i og album.length - altså istedet for
+//at skrive 0 < 14
   for (let i=0 ; i < albums.length; i++) {
     const album = new Album (
         albums[i].albumName,
@@ -25,9 +27,21 @@ let albumObjects = []
         albums[i].productionYear
     );
 
-    //sætter det ind i arrayet
+    //sætter det ind i det tomme array "albumObjects"
     albumObjects.push(album);
   };
+  //Laver et loop der sætter de 4 ting ind på hjemmesiden
+  //document.getElementById("Musikarkiv").innerHTML = albumObjects[0].albumName
+  const tbody=document.getElementById("musikarkiv").getElementsByTagName("tbody")[0];
+  for (let i=0 ; i < albumObjects.length; i++) {
+    const tr=document.getElementById("tr");
+
+    const tdalbumName=document.createElement("td");
+    tdalbumName.textcontent=albumObjects[i].albumName;
+
+    tr.appendChild(tdalbumName);
+    tbody.appendChild(tr);
+  }
 });
 
 //generelt fortæller async at koden ikke behøver vente til done
@@ -36,3 +50,9 @@ async function fetchContent(url) {
     let json = await request.json();
     return json;
   }
+
+
+
+  
+
+
